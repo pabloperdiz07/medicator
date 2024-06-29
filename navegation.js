1064704cd45a3ef15734eeb97cd4f7de800e1a05
Pablo Perdiz <pabloalfayaperdiz@gmail.com>
2024-06-29 00:01:49 -0300

    feat: adding resonsivity

---
+++ navegation.js
@@ -0,0 +1,7 @@
+function navMobileClicked() {
+    document.getElementById("mobile-nav-box").style.display = 'flex'
+}
+
+function exitNavMobile() {
+    document.getElementById("mobile-nav-box").style.display = 'none'
+}
